7295a999fa240046b3db8da84424b797fdd51fae
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-04-17 21:53:36 +0200

    add tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function App() {
   return (
     <>
-      <h1>Planning Poker 🚀</h1>
+      <h1 className="text-3xl font-bold">Planning Poker 🚀</h1>
       <p>Using React, Typescript and Vite </p>
     </>
   );
